06713686457b488a7fb38512656e644f3c24d42f
majid <majidted@gmail.com>
2020-12-25 18:46:03 +0330

    feat: rocket mouse character atlas

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -1,7 +1,8 @@
 import Phaser from 'phaser'
 
 enum GameImages {
-    Background = "background"
+    Background = "background",
+    RocketMouse = "rocket-mouse"
 }
 
 export default class Game extends Phaser.Scene {
@@ -10,11 +11,19 @@ export default class Game extends Phaser.Scene {
     }
 
     preload() {
-        this.load.image(GameImages.Background, "house/bg_repeat_340x640.png")
+        this.load.image(GameImages.Background, "house/bg_repeat_340x640.png");
+        this.load.atlas(GameImages.RocketMouse, "characters/rocket-mouse.png", "characters/rocket-mouse.json");
     }
 
     create() {
         const { width, height } = this.scale;
-        this.add.tileSprite(0, 0, width, height, GameImages.Background).setOrigin(0);
+        this.add.tileSprite(0, 0, width, height, GameImages.Background)
+            .setOrigin(0);
+        this.add.sprite(
+            width * 0.5,
+            height * 0.5,
+            GameImages.RocketMouse,
+            "rocketmouse_fly01.png"
+        );
     }
 }

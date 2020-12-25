e1f5dd4d7c31557102c609921d50314f69c4b2b2
majid <majidted@gmail.com>
2020-12-25 20:27:51 +0330

    feat: camera following mouse

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -24,10 +24,19 @@ export default class Game extends Phaser.Scene {
         const body = mouse.body as Phaser.Physics.Arcade.Body;
         body.setCollideWorldBounds(true);
 
+        body.setVelocityX(200);
+
         this.physics.world.setBounds(
             0, 0,
             Number.MAX_SAFE_INTEGER,
             height - 30
+        );
+
+        this.cameras.main.startFollow(mouse);
+        this.cameras.main.setBounds(
+            0, 0,
+            Number.MAX_SAFE_INTEGER,
+            height
         );
     }
 }

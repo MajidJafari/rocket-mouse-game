560e2af25af2e18526ef8cc408dd1857c49181fc
majid <majidted@gmail.com>
2020-12-31 14:05:11 +0330

    feat: RocketMouse class refactored

--- src/game/RocketMouse.ts
+++ src/game/RocketMouse.ts
@@ -3,31 +3,37 @@ import TextuerKeys from '~/consts/TextureKeys';
 import AnimationKeys from '~/consts/AnimationKeys';
 
 export default class RocketMouse extends Phaser.GameObjects.Container {
+    mouse!: Phaser.GameObjects.Sprite;
+    flames!: Phaser.GameObjects.Sprite;
     constructor(scene: Phaser.Scene, x: number, y: number) {
         super(scene, x, y);
 
         // create the flames and play the animation
         this.addFlames(scene);
         this.addMouse(scene);
+        this.handlePhysics(scene);
         
     }
 
-    private addFlames(scene: Phaser.Scene) {
-        const flames = scene.add.sprite(-63, -15, TextuerKeys.RocketMouse)
-            .play(AnimationKeys.RocketFlamesOn);
-        this.add(flames);
-    }
-
-    private addMouse(scene: Phaser.Scene) {
-        const mouse = scene.add.sprite(0, 0, TextuerKeys.RocketMouse)
-            .setOrigin(0.5, 1)
-            .play(AnimationKeys.RocketMouseRun);
-
-        this.add(mouse);
+    private handlePhysics(scene: Phaser.Scene) {
         scene.physics.add.existing(this);
-
+        const mouse = this.mouse;
         const body = this.body as Phaser.Physics.Arcade.Body;
         body.setSize(mouse.width, mouse.height);
         body.setOffset(mouse.width * -0.5, -mouse.height);
+    }
+
+    private addFlames(scene: Phaser.Scene) {
+        this.flames = scene.add.sprite(-63, -15, TextuerKeys.RocketMouse)
+            .play(AnimationKeys.RocketFlamesOn);
+        this.add(this.flames);
+    }
+
+    private addMouse(scene: Phaser.Scene) {
+        this.mouse = scene.add.sprite(0, 0, TextuerKeys.RocketMouse)
+            .setOrigin(0.5, 1)
+            .play(AnimationKeys.RocketMouseRun);
+
+        this.add(this.mouse);
     }
 }
